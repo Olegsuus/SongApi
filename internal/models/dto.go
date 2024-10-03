760b6e35978457c8641b63711397dbce54532333
Oleg Semashko <semashko.olezhka@inbox.ru>
2024-10-03 09:53:04 +0300

    swagg

--- internal/models/dto.go
+++ internal/models/dto.go
@@ -1,18 +1,5 @@
 package models
 
-//type AddSongDTO struct {
-//	Group string `json:"group"`
-//	Song  string `json:"song"`
-//}
-//
-//type UpdateSongDTO struct {
-//	Group       string `json:"group,omitempty"`
-//	Song        string `json:"song,omitempty"`
-//	ReleaseDate string `json:"release_date,omitempty"`
-//	Text        string `json:"text,omitempty"`
-//	Link        string `json:"link,omitempty"`
-//}
-
 type UpdateSongDTO struct {
 	Group       string `json:"group,omitempty" validate:"omitempty,max=255,not_russian"`
 	Song        string `json:"song,omitempty" validate:"omitempty,max=255,not_russian"`
